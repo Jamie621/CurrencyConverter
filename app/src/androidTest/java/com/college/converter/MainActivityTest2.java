b834d7ead7581ba0d23e04222631c0efa321322d
Fei Wu <wu000220@algonquinlive.com>
2024-02-14 21:54:27 -0500

    week 6: MERGE AGAIN

--- app/src/androidTest/java/com/college/converter/MainActivityTest2.java
+++ app/src/androidTest/java/com/college/converter/MainActivityTest2.java
@@ -1,6 +1,5 @@
 package com.college.converter;
 
-
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
